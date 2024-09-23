4336fde493efdb2f2de44c6700a375eaa06a62d9
Tatiana Molchanova <tatuka.08@mail.ru>
2024-09-23 18:08:17 -0400

    aiohttp

--- models.py
+++ models.py
@@ -8,9 +8,9 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, WriteOnlyMapp
 
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5431")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "app")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "app")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "secret")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
 
 PG_DSN = (
     f"postgresql+asyncpg://"
